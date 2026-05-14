44239494d35c3bb7b23cf4565a441b3870feb653
Eng_Amrolela <amrolela100@gmail.com>
2026-05-14 15:48:09 -0400

    chore(cleanup): remove .migration-backup migration.sql

--- .migration-backup/prisma/migrations/20260330194452_add_analysis_rich_fields/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Analysis" ADD COLUMN     "competitors" JSONB DEFAULT '[]',
-ADD COLUMN     "revenueImpact" JSONB DEFAULT '{}',
-ADD COLUMN     "serviceGaps" JSONB DEFAULT '[]',
-ADD COLUMN     "socialPresence" JSONB DEFAULT '{}';
